4e2f468ead30beca112737a7f64c220f1da9b72d
Sarah-mp <126619525+Sarah-mp@users.noreply.github.com>
2024-02-10 16:02:12 +0000

    cambios

--- clase1/main.py
+++ clase1/main.py
@@ -1,1 +1,12 @@
-print("primer codigo en python")
+class MotorElectrico:
+    potencia = None
+
+    def encende(self):
+        print("Encendiendo motor eléctrico")
+
+        def__init__(self):
+            pass
+
+motor1 = MotorElectrico()
+motor1.potencia=1000
+motor1.encender()
