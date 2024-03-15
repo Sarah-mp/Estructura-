9d3f25a54c8b4d685cdbac6a262dc4a19f9bb73f
Sarah-mp <126619525+Sarah-mp@users.noreply.github.com>
2024-03-15 13:19:53 +0000

    cambios

---
+++ clase1/clase5.py
@@ -0,0 +1,21 @@
+a=3
+b=3
+
+print(type(b))
+
+class Nodo:
+  def __int__(self, valor):
+    self.valor=valor
+    self.sigueinte= None
+
+    nodo1= Nodo(5)
+    nodo2= Nodo(5)
+
+    print(type(nodo2))
+    print('Nodo 2')
+    print(type(nodo1))
+    print('Nodo 2')
+
+     
+
+
