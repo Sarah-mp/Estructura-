f78f30e0563dce5442fc554a899ccd15d36dd79d
Sarah-mp <kirapra2017@gmail.com>
2024-05-19 17:38:35 -0500

    cambios

--- Trabajo final/conjunto_de_datos/arbol_binario.py
+++ Trabajo final/conjunto_de_datos/arbol_binario.py
@@ -5,21 +5,53 @@ class Nodo:
         self.modelo = modelo #Guarda el modelo de clasificación en la instancia del no
         self.correo = correo # Almacena el correo electrónico en el nodo.
         self.es_spam = es_spam #  Almacena la clasificación del correo (spam o no).
-        self.izquierda = None # Inicializa el hijo izquierdo del nodo.
-        self.derecha = None # Inicializa el hijo derecho del nodo.
+        self.izquiero = None
+        self.derecha = None
 
+
+class ArbolSpam:
+    izquierda = None # Inicializa el hijo izquierdo del nodo.
+    derecha = None # Inicializa el hijo derecho del nodo.
+    modelo = None
+    def __init__(self, raiz=None):
+        self.raiz = raiz
+        
     def evaluar_correo(self, correo):
         # Usar el modelo para predecir si el correo es spam o no
        
         prediccion = self.modelo.predict([correo])[0] # devuelve una lista, obtiene el resultado para el correo dado.
         return prediccion  
+    
+    def _insertar(self, valor, nodo):
+        if valor < nodo.es_spam:
+            if nodo.izquierda is None:
+                nodo.izquierda = Nodo(valor)
+            else:
+                self._insertar(valor, nodo.izquierda)
+        else:
+            if nodo.derecha is None:
+                nodo.derecha = Nodo(valor)
+            else:
+                self._insertar(valor, nodo.derecha)
 
     def insertar(self, correo):  #  Define cómo insertar un nuevo correo en el árbol.
+        #  Si el nodo actual no tiene correo, lo asigna y evalúa si es spam.
+        self.correo = correo # Asigna el correo al nodo.
+        self.es_spam = self.evaluar_correo(correo)  #Evalúa el correo y almacena el resultado.
+        nodo = Nodo(self.modelo, correo, self.es_spam) 
+        print("="*50)
+        print(correo)
+        self._insertar(self.es_spam, nodo)
+        
+    """
+    def insertar_copia(self, correo):  #  Define cómo insertar un nuevo correo en el árbol.
         if self.correo is None: #  Si el nodo actual no tiene correo, lo asigna y evalúa si es spam.
             self.correo = correo # Asigna el correo al nodo.
             self.es_spam = self.evaluar_correo(correo)  #Evalúa el correo y almacena el resultado.
         else:
-           
+            
+            print("="*50)
+            print(correo)
             if self.es_spam == 1:  # Si el correo en el nodo es spam, intenta insertar el nuevo correo en el subárbol derecho.
                 if self.derecha is None: # Si no hay hijo derecho, crea un nuevo nodo.
                     self.derecha = Nodo(self.modelo) 
@@ -29,7 +61,7 @@ class Nodo:
                 if self.izquierda is None:
                     self.izquierda = Nodo(self.modelo)
                 self.izquierda.insertar(correo)
-
+    """
     def imprimir(self):  # Define cómo imprimir todos los correos en el árbol.
         if self.izquierda:  # Si hay un subárbol izquierdo, lo imprime primero (recorrido inorden
             self.izquierda.imprimir()
--- Trabajo final/conjunto_de_datos/prueba_binarios.py
+++ Trabajo final/conjunto_de_datos/prueba_binarios.py
@@ -2,26 +2,29 @@ import sys  # proporciona acceso a algunas variables y funciones
 sys.setrecursionlimit(10000)  # aumenta el límite de recursión predeterminado del intérprete de Python 
 import pandas as pd
 from modelo import clf
-from arbol_binario import Nodo 
+from arbol_binario import ArbolSpam
 
 df = pd.read_csv('spam.csv')
 
  #Supongamos que estos son ejemplos de correos para probar
 correos_de_prueba = [
-    "Congratulations! you've won a $1000 Walmart gift card. Go to http://example.com to claim now.",
-    "Hi, I hope you are doing well. We have a meeting scheduled for 3 PM today.",
-    "Urgent: Your account has been breached. Immediate action required!",
-    "Hello, don't forget the book club meeting tomorrow morning.",
-    "Earn money fast! Guaranteed high returns with minimal investment. Click here!"
-    "hjbshsaksjskddksjkbjmbssjkbdjsasbjhsbdx"
+    "BB/LIVELO Caro Sr(a) seus pontos estão prestes a expirar. Notificação 9582777074871504- Para mais informações, acesse o anexo.",
+    "Hola sara, tengo unas malas noticias con su compañero sergio. este muchacho no da pie con bola"
+    #"Congratulations! you've won a $1000 Walmart gift card. Go to http://example.com to claim now.",
+    #"Hi, I hope you are doing well. We have a meeting scheduled for 3 PM today.",
+    #"Urgent: Your account has been breached. Immediate action required!",
+    #"Hello, don't forget the book club meeting tomorrow morning.",
+    #"Earn money fast! Guaranteed high returns with minimal investment. Click here!"
+    #"hjbshsaksjskddksjkbjmbssjkbdjsasbjhsbdx"
     
 ]
 
-correos_de_prueba = list (df["Message"]) # todos los correos del archivo CSV se utilizan para las pruebas.
+# correos_de_prueba = list (df["Message"]) # todos los correos del archivo CSV se utilizan para las pruebas.
 
 
 # Se pasa el modelo de clasificación clf a este nodo, el cual se usará para evaluar si cada correo es spam o no.
-raiz = Nodo(clf)
+raiz = ArbolSpam()
+raiz.modelo = clf
 
 # Insertar cada correo en el árbol y evaluar si es spam o no
 for correo in correos_de_prueba: #  Itera sobre cada correo en la lista 
